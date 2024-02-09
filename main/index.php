c6ac6143edb7420827193334b089796612460fa0
KenEstander <kenestander@gmail.com>
2024-02-09 16:11:47 +0800

    updated file

--- main/index.php
+++ main/index.php
@@ -47,15 +47,45 @@ ASDASDHAKJSHDJAKSHDJASDHKJ
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Inquire Now</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <form action="process.php?action=inquiry-add" method="post">
+  <div class="row">
+    <div class="col-6">
+        <input type="text" name="firstName" placeholder="Firstname*" class="form-control">
+    </div>
+    <div class="col-6">
+        <input type="text" name="lastName" placeholder="Lastname*" class="form-control">
+    </div>
+    <div class="col-12 mt-2">
+        <input type="text" name="company" placeholder="Company(optional)*" class="form-control">
+    </div>
+    <div class="col-6 mt-2">
+        <input type="text" name="email" placeholder="Email Address*" class="form-control">
+    </div>
+    <div class="col-6 mt-2">
+      <input type="text" name="phone" placeholder="Contact Number*" class="form-control">
+    </div>
+
+    <div class="col-12 mt-5">
+      <select class="form-control" name="type" required>
+      <option value="">--Select--</option>
+      <option>Website</option>
+      <option>Apps</option>
+      </select>
+    </div>
+    <div class="col-12 mt-2">
+      <textarea  class="form-control" type="text" name="message" placeholder="Message*" required>
+      </textarea>
+      <br>
+      <button type="submit" name="button" class="btn btn-primary">SUBMIT</button>
+
+    </div>
+          </form>
+
+
       </div>
     </div>
   </div>
--- main/process.php
+++ main/process.php
@@ -7,11 +7,33 @@ $action = $_GET['action'];
 
 switch ($action) {
 
-	case 'record-add' :
-		record_add();
-		break;
+		case 'record-add' :
+			record_add();
+			break;
+
+		case 'inquiry-add' :
+			inquiry_add();
+			break;
+
 
 	default :
+}
+
+
+function inquiry_add()
+{
+	$model = inquiry();
+	$model->obj["firstName"] = $_POST["firstName"];
+	$model->obj["lastName"] = $_POST["lastName"];
+	$model->obj["company"] = $_POST["company"];
+	$model->obj["email"] = $_POST["email"];
+	$model->obj["phone"] = $_POST["phone"];
+	$model->obj["type"] = $_POST["type"];
+	$model->obj["message"] = $_POST["message"];
+
+	$model->create();
+
+header('Location: index.php?success=Your inquiry will be added to our database');
 }
 
 function delete_records(){
