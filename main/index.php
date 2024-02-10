616f4a988f4acff6f764924300d3ccbc94297696
KenEstander <kenestander@gmail.com>
2024-02-10 19:12:06 +0800

    new branch copy

--- main/index.php
+++ main/index.php
@@ -27,36 +27,9 @@
                     </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img class="w-100" src="<?=$ROOT_DIR;?>templates/img/carousel-2.jpg" alt="Image">
-                <div class="carousel-caption">
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-7 pt-5">
-                                <h1 class="display-4 text-white mb-3 animated slideInDown">There's no other place, there's no other place</h1>
-                                <p class="fs-5 text-white-50 mb-5 animated slideInDown">Aliqu diam amet diam et eos. Clita erat ipsum et lorem sed stet lorem sit clita duo justo erat amet</p>
-                                <a class="btn btn-primary py-2 px-3 animated slideInDown" href="">
-                                    Learn More
-                                    <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
-                                        <i class="fa fa-arrow-right"></i>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel"
-            data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#header-carousel"
-            data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+
     </div>
 </section>
 <!-- Carousel End -->
