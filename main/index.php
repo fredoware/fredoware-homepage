48f9fbe1b0cf32218aab49b0e2b89b58287ceecb
fredowinz23 <fredowinz23@gmail.com>
2024-02-10 21:28:33 +0800

    Added structure

--- main/index.php
+++ main/index.php
@@ -14,8 +14,8 @@
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-lg-7 pt-5">
-                                <h1 class="display-4 text-white mb-3 animated slideInDown">We Make Ideas Happen.</h1>
-                                <p class="fs-5 text-white-50 mb-5 animated slideInDown">Aliqu diam amet diam et eos. Clita erat ipsum et lorem sed stet lorem sit clita duo justo erat amet</p>
+                                <h1 class="display-5 text-white mb-3 animated slideInDown">Innovate, Create, Dominate</h1>
+                                <p class="fs-5 text-white-50 mb-5 animated slideInDown">Designing your digital success story.</p>
                                 <a class="btn btn-primary py-2 px-3 animated slideInDown" href="">
                                     Start a project
                                     <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
@@ -37,7 +37,48 @@
 
 <!-- About us Start -->
 <section class="container-fluid p-0 mb-5" id="aboutus">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative overflow-hidden h-100" style="min-height: 400px;">
+                    <img class="position-absolute w-100 h-100 pt-5 pe-5" src="<?=$ROOT_DIR;?>templates/img/about-1.jpg" alt="" style="object-fit: cover;">
+                    <img class="position-absolute top-0 end-0 bg-white ps-2 pb-2" src="<?=$ROOT_DIR;?>templates/img/about-2.jpg" alt="" style="width: 200px; height: 200px;">
+                </div>
+            </div>
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="h-100">
+                    <h1 class="fs-5">Welcome to</h1>
+                    <h1 class="fs-2 mb-3 primary-color">Fredoware Software Services</h1>
 
+                    <p>
+                    We are your premier destination for comprehensive IT solutions encompassing web and mobile app development. At FSS, we pride ourselves on delivering cutting-edge technology solutions tailored to meet the diverse needs of our clients.
+                    </p>
+                    <p>
+                    With a team of skilled developers and designers, we specialize in crafting stunning websites and mobile applications that not only meet but exceed industry standards. From sleek and responsive web designs to intuitive and user-friendly mobile apps, we're committed to bringing your digital vision to life.
+                    </p>
+                    <p>
+                    Our approach is rooted in collaboration and innovation, ensuring that every project we undertake is guided by your unique goals and requirements. Whether you're a startup looking to establish your online presence or a large corporation seeking to streamline your mobile strategy, we're here to help you every step of the way.
+                    </p>
+                    <p>
+                    At FSS, we're not just in the business of building websites and mobile apps – we're in the business of building relationships. Our dedication to exceptional customer service and client satisfaction sets us apart, making us your trusted partner in IT solutions. Contact us today and let's embark on a journey of digital transformation together.
+                    </p>
+
+                  <a class="btn btn-primary py-2 px-3 me-3" href="">
+                        Learn More
+                        <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
+                            <i class="fa fa-arrow-right"></i>
+                        </div>
+                    </a>
+                    <a class="btn btn-outline-primary py-2 px-3" href="">
+                        Contact Us
+                        <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                            <i class="fa fa-arrow-right"></i>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <!-- About us End -->
 
@@ -45,8 +86,129 @@
 
 
 <!-- Services Start -->
-<section class="container-fluid p-0 mb-5" id="services">
+<section class="container-fluid p-5 mb-5 section-dark" id="services">
 
+      <div class="container">
+          <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+              <h1 class="display-6 mb-5">Our Services</h1>
+          </div>
+          <div class="row g-4 justify-content-center">
+
+              <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                  <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
+                      <h4 class="mb-3">Web Development Services</h4>
+                      Custom Website Development <br>
+                      Custom Website Development <br>
+                      Custom Website Development <br>
+                      Custom Website Development <br>
+                      Custom Website Development <br>
+                      Custom Website Development <br>
+                      Custom Website Development <br>
+
+                      <a class="btn btn-outline-primary mt-3 px-3" href="">
+                          Learn More
+                          <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                              <i class="fa fa-arrow-right"></i>
+                          </div>
+                      </a>
+                  </div>
+              </div>
+
+
+
+                  <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                      <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
+                          <h4 class="mb-3">Web Development Services</h4>
+                          Custom Website Development <br>
+                          Custom Website Development <br>
+                          Custom Website Development <br>
+                          Custom Website Development <br>
+                          Custom Website Development <br>
+                          Custom Website Development <br>
+                          Custom Website Development <br>
+
+                          <a class="btn btn-outline-primary mt-3 px-3" href="">
+                              Learn More
+                              <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                                  <i class="fa fa-arrow-right"></i>
+                              </div>
+                          </a>
+                      </div>
+                  </div>
+
+
+
+                      <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                          <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
+                              <h4 class="mb-3">Web Development Services</h4>
+                              Custom Website Development <br>
+                              Custom Website Development <br>
+                              Custom Website Development <br>
+                              Custom Website Development <br>
+                              Custom Website Development <br>
+                              Custom Website Development <br>
+                              Custom Website Development <br>
+
+                              <a class="btn btn-outline-primary mt-3 px-3" href="">
+                                  Learn More
+                                  <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                                      <i class="fa fa-arrow-right"></i>
+                                  </div>
+                              </a>
+                          </div>
+                      </div>
+
+
+
+                          <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                              <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
+                                  <h4 class="mb-3">Web Development Services</h4>
+                                  Custom Website Development <br>
+                                  Custom Website Development <br>
+                                  Custom Website Development <br>
+                                  Custom Website Development <br>
+                                  Custom Website Development <br>
+                                  Custom Website Development <br>
+                                  Custom Website Development <br>
+
+                                  <a class="btn btn-outline-primary mt-3 px-3" href="">
+                                      Learn More
+                                      <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                                          <i class="fa fa-arrow-right"></i>
+                                      </div>
+                                  </a>
+                              </div>
+                          </div>
+
+
+
+                              <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                                  <div class="service-item bg-white text-center h-100 p-4 p-xl-5">
+                                      <h4 class="mb-3">Web Development Services</h4>
+                                      Custom Website Development <br>
+                                      Custom Website Development <br>
+                                      Custom Website Development <br>
+                                      Custom Website Development <br>
+                                      Custom Website Development <br>
+                                      Custom Website Development <br>
+                                      Custom Website Development <br>
+
+                                      <a class="btn btn-outline-primary mt-3 px-3" href="">
+                                          Learn More
+                                          <div class="d-inline-flex btn-sm-square bg-primary text-white rounded-circle ms-2">
+                                              <i class="fa fa-arrow-right"></i>
+                                          </div>
+                                      </a>
+                                  </div>
+                              </div>
+
+
+
+
+
+
+          </div>
+      </div>
 </section>
 <!-- Services End -->
 
@@ -54,14 +216,60 @@
 
 <!-- Website Start -->
 <section class="container-fluid p-0 mb-5" id="websites">
-
+  <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+          <div class="carousel-item active">
+              <img class="w-100" src="<?=$ROOT_DIR;?>templates/img/web.jpg" alt="Image">
+              <div class="carousel-caption">
+                  <div class="container">
+                      <div class="row">
+                          <div class="col-lg-6 pt-5" style="text-align:left">
+                              <p class="fs-2 text-white-50 mb-2 animated slideInDown">Website Development</p>
+                              <h1 class="display-4 text-white mb-3 animated slideInDown">Websites.</h1>
+                              <p class="fs-5 text-white-50 mb-5 animated slideInDown">Aliqu diam amet diam et eos. Clita erat ipsum et lorem sed stet lorem sit clita duo justo erat amet</p>
+                              <a class="btn btn-primary py-2 px-3 animated slideInDown" href="">
+                                  Start a project
+                                  <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
+                                      <i class="fa fa-arrow-right"></i>
+                                  </div>
+                              </a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </section>
 <!-- Website End -->
 
 
 <!-- App Start -->
 <section class="container-fluid p-0 mb-5" id="apps">
-
+  <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+          <div class="carousel-item active">
+              <img class="w-100" src="<?=$ROOT_DIR;?>templates/img/web.jpg" alt="Image">
+              <div class="carousel-caption">
+                  <div class="container">
+                      <div class="row">
+                          <div class="col-lg-12 pt-5" style="text-align:right">
+                              <p class="fs-2 text-white-50 mb-2 animated slideInDown">Mobile App Development</p>
+                              <h1 class="display-4 text-white mb-3 animated slideInDown">Apps.</h1>
+                              <p class="fs-5 text-white-50 mb-5 animated slideInDown">Aliqu diam amet diam et eos. Clita erat ipsum et lorem sed stet lorem sit clita duo justo erat amet</p>
+                              <a class="btn btn-primary py-2 px-3 animated slideInDown" href="">
+                                  Start a project
+                                  <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
+                                      <i class="fa fa-arrow-right"></i>
+                                  </div>
+                              </a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 </section>
 <!-- App End -->
 
@@ -69,7 +277,51 @@
 
 <!-- Contact us Start -->
 <section class="container-fluid p-0 mb-5" id="contactus">
-
+  <div class="container">
+      <div class="row g-5">
+          <div class="col-lg-12 wow fadeIn" data-wow-delay="0.1s">
+              <div class="d-inline-block rounded-pill bg-secondary text-primary py-1 px-3 mb-3">Contact Us</div>
+              <h1 class="display-6 mb-5">If You Have Any Query, Please Contact Us</h1>
+              <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
+              <form>
+                  <div class="row g-3">
+                      <div class="col-md-6">
+                          <div class="form-floating">
+                              <input type="text" class="form-control" id="name" placeholder="Your Name">
+                              <label for="name">Your Name</label>
+                          </div>
+                      </div>
+                      <div class="col-md-6">
+                          <div class="form-floating">
+                              <input type="email" class="form-control" id="email" placeholder="Your Email">
+                              <label for="email">Your Email</label>
+                          </div>
+                      </div>
+                      <div class="col-12">
+                          <div class="form-floating">
+                              <input type="text" class="form-control" id="subject" placeholder="Subject">
+                              <label for="subject">Subject</label>
+                          </div>
+                      </div>
+                      <div class="col-12">
+                          <div class="form-floating">
+                              <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 100px"></textarea>
+                              <label for="message">Message</label>
+                          </div>
+                      </div>
+                      <div class="col-12">
+                          <button class="btn btn-primary py-2 px-3 me-3">
+                              Send Message
+                              <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
+                                  <i class="fa fa-arrow-right"></i>
+                              </div>
+                          </button>
+                      </div>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
 </section>
 <!-- Contact us End -->
 
