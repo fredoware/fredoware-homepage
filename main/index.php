f13e1a3e849f70eebb84d95bafae2b6922f81ce8
KenEstander <kenestander@gmail.com>
2024-02-10 19:21:44 +0800

    images for website

--- main/index.php
+++ main/index.php
@@ -9,15 +9,15 @@
     <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="w-100" src="<?=$ROOT_DIR;?>templates/img/carousel-2.jpg" alt="Image">
+                <img class="w-100" src="<?=$ROOT_DIR;?>templates/img/home.jpg" alt="Image">
                 <div class="carousel-caption">
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-lg-7 pt-5">
-                                <h1 class="display-4 text-white mb-3 animated slideInDown">We Find Ways. We got it all for you.</h1>
+                                <h1 class="display-4 text-white mb-3 animated slideInDown">We Make Ideas Happen.</h1>
                                 <p class="fs-5 text-white-50 mb-5 animated slideInDown">Aliqu diam amet diam et eos. Clita erat ipsum et lorem sed stet lorem sit clita duo justo erat amet</p>
                                 <a class="btn btn-primary py-2 px-3 animated slideInDown" href="">
-                                    Learn More
+                                    Start a project
                                     <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
                                         <i class="fa fa-arrow-right"></i>
                                     </div>
@@ -27,7 +27,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
 
     </div>
