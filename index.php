97c9683b40237d582905d402b1852b15bc67daf2
fredowinz23 <fredowinz23@gmail.com>
2025-02-23 00:02:06 +0800

    Added main

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Fredoware Software solutions</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta property="og:title" content="Fredoware Software Solutions" />
+  <meta property="og:url" content="https://fredoware.com" />
+  <meta property="og:image" content="assets/img/og.png" />
+  
+</head>
 <?php
   header("Location: web/");
 ?>
